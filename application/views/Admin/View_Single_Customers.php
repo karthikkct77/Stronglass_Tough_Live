526b1347d69c88419d4d8824e39e8d997de624c7
IdeaBricks <karthik@ibtemail.com>
2018-05-03 09:45:55 +0530

    saffdsaf

--- application/views/Admin/View_Single_Customers.php
+++ application/views/Admin/View_Single_Customers.php
@@ -7,7 +7,7 @@
         </div>
         <ul class="app-breadcrumb breadcrumb">
             <li class="breadcrumb-item"><i class="fa fa-home fa-lg"></i></li>
-            <li class="breadcrumb-item"><a href="<?php echo site_url('Admin_Controller/Add_Customers'); ?>">Customer Entry</a></li>
+            <li class="breadcrumb-item"><a href="<?php echo site_url('Admin_Controller/View_Customers'); ?>">Customer List</a></li>
             <li class="breadcrumb-item"><a href="#">View Customers</a></li>
         </ul>
     </div>
