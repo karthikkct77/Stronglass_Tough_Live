942afbc99ca56e332fcb9bc2e8c258c4a2b2e131
IdeaBricks <karthik@ibtemail.com>
2018-05-17 10:34:24 +0530

    saffdsaf

--- application/views/User/left.php
+++ application/views/User/left.php
@@ -17,14 +17,61 @@
             { ?>
                 <p class="app-sidebar__user-designation">DISPATCH</p>
           <?php  }
+            elseif ($_SESSION['role'] == 5)
+            { ?>
+                <p class="app-sidebar__user-designation">DATA ENTRY</p>
+            <?php  }
+            elseif ($_SESSION['role'] == 6)
+            { ?>
+                <p class="app-sidebar__user-designation">WO ENTRY</p>
+            <?php  }
+            elseif ($_SESSION['role'] == 6)
+            { ?>
+                <p class="app-sidebar__user-designation">QA</p>
+            <?php  }
             ?>
 
         </div>
     </div>
     <ul class="app-menu">
-        <li><a class="app-menu__item active" href="<?php echo site_url('User_Controller/dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
 
-        <li><a class="app-menu__item" href="<?php echo site_url('User_Controller/Work_Order'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Work Order</span></a></li>
+        <?php
+        if($_SESSION['role'] == 2)
+        { ?>
+            <li><a class="app-menu__item active" href="<?php echo site_url('User_Controller/dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+            <li><a class="app-menu__item" href="<?php echo site_url('User_Controller/Work_Order'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Work Order</span></a></li>
 
+        <?php   }
+        elseif ($_SESSION['role'] == 3)
+        { ?>
+            <li><a class="app-menu__item active" href="<?php echo site_url('User_Controller/dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+            <li><a class="app-menu__item" href="<?php echo site_url('User_Controller/Work_Order'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Work Order</span></a></li>
+
+        <?php  }
+        elseif ($_SESSION['role'] == 4)
+        { ?>
+            <li><a class="app-menu__item active" href="<?php echo site_url('User_Controller/dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+            <li><a class="app-menu__item" href="<?php echo site_url('User_Controller/Work_Order'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Work Order</span></a></li>
+
+        <?php  }
+        elseif ($_SESSION['role'] == 5)
+        { ?>
+            <li><a class="app-menu__item active" href="<?php echo site_url('User_Controller/dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+            <li><a class="app-menu__item" href="<?php echo site_url('User_Controller/Proforma_Invoice'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Create PI</span></a></li>
+
+        <?php  }
+        elseif ($_SESSION['role'] == 6)
+        { ?>
+            <li><a class="app-menu__item active" href="<?php echo site_url('User_Controller/dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+            <li><a class="app-menu__item" href="<?php echo site_url('User_Controller/Generate_WO'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Create WO</span></a></li>
+
+        <?php  }
+        elseif ($_SESSION['role'] == 6)
+        { ?>
+            <li><a class="app-menu__item active" href="<?php echo site_url('User_Controller/dashboard'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">Dashboard</span></a></li>
+            <li><a class="app-menu__item" href="<?php echo site_url('User_Controller/Check_WO'); ?>"><i class="app-menu__icon fa fa-dashboard"></i><span class="app-menu__label">CHECK WO</span></a></li>
+
+        <?php  }
+        ?>
     </ul>
 </aside>
