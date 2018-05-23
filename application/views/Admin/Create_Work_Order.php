4f714bf46948edde48a22dcec42e0b885c42ccf4
IdeaBricks <karthik@ibtemail.com>
2018-05-23 15:11:59 +0530

    number to words

--- application/views/Admin/Create_Work_Order.php
+++ application/views/Admin/Create_Work_Order.php
@@ -238,6 +238,7 @@
                                 </tfoot>
                             </table>
                         </div>
+                        <div>Amount in Words: <span id="word"></span></div>
                         <script>
 //                            $("#insurance").on('change keyup paste', function() {
 //                                var sub_tot =document.getElementById('sub_tot').value;
@@ -294,6 +295,7 @@
         position: absolute;
     }
 </style>
+
 
 
 <script>
@@ -833,6 +835,56 @@
         document.getElementById('igst').value = parseFloat(sum).toFixed(2);
         var grant = (parseFloat(sub_tot) + parseFloat(insurance) + parseFloat(trans) + parseFloat(sum));
         document.getElementById('gross_tot').value = parseInt(grant);
+
+        var th = ['', 'thousand', 'million', 'billion', 'trillion'];
+
+        var dg = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'];
+
+        var tn = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+
+        var tw = ['twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+
+        var s = document.getElementById('gross_tot').value;
+
+        s = s.toString();
+        s = s.replace(/[\, ]/g, '');
+        if (s != parseFloat(s)) return 'not a number';
+        var x = s.indexOf('.');
+        if (x == -1) x = s.length;
+        if (x > 15) return 'too big';
+        var n = s.split('');
+        var str = '';
+        var sk = 0;
+        for (var i = 0; i < x; i++) {
+            if ((x - i) % 3 == 2) {
+                if (n[i] == '1') {
+                    str += tn[Number(n[i + 1])] + ' ';
+                    i++;
+                    sk = 1;
+                } else if (n[i] != 0) {
+                    str += tw[n[i] - 2] + ' ';
+                    sk = 1;
+                }
+            } else if (n[i] != 0) {
+                str += dg[n[i]] + ' ';
+                if ((x - i) % 3 == 0) str += 'hundred ';
+                sk = 1;
+            }
+            if ((x - i) % 3 == 1) {
+                if (sk) str += th[(x - i - 1) / 3] + ' ';
+                sk = 0;
+            }
+        }
+        document.getElementById('word').innerHTML = str;
+        if (x != s.length) {
+            var y = s.length;
+            str += 'point ';
+            for (var i = x + 1; i < y; i++) str += dg[n[i]] + ' ';
+        }
+        return str.replace(/\s+/g, ' ');
+
+
+
     }
     function GST() {
         $('#igst1').hide();
