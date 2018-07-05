e2e2fb3973a2890915cdabc2ae85f710778347fa
karthik <karthikkct77@gmail.com>
2018-07-05 13:11:06 +0530

    testing

--- application/views/User/View_Invoice.php
+++ application/views/User/View_Invoice.php
@@ -310,6 +310,17 @@
         border-radius: 5px 5px 5px 5px;
         position: absolute;
     }
+
+    /* For Firefox */
+    input[type='number'] {
+        -moz-appearance:textfield;
+    }
+    /* Webkit browsers like Safari and Chrome */
+    input[type=number]::-webkit-inner-spin-button,
+    input[type=number]::-webkit-outer-spin-button {
+        -webkit-appearance: none;
+        margin: 0;
+    }
 </style>
 
 
